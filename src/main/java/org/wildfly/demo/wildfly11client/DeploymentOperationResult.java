d8b4564dc4c0ccb1c52771184352452cd572276a
Rob Stryker <rob@oxbeef.net>
2017-04-04 18:24:17 -0400

    Cleanup

--- src/main/java/org/wildfly/demo/wildfly11client/DeploymentOperationResult.java
+++ src/main/java/org/wildfly/demo/wildfly11client/DeploymentOperationResult.java
@@ -1,13 +1,14 @@
-/******************************************************************************* 
- * Copyright (c) 2011 Red Hat, Inc. 
- * Distributed under license by Red Hat, Inc. All rights reserved. 
- * This program is made available under the terms of the 
- * Eclipse Public License v1.0 which accompanies this distribution, 
- * and is available at http://www.eclipse.org/legal/epl-v10.html 
- * 
- * Contributors: 
- * Red Hat, Inc. - initial API and implementation 
- ******************************************************************************/ 
+/*******************************************************************************
+ * Copyright (c) 2011 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.wildfly.demo.wildfly11client;
 
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -18,51 +19,53 @@ import org.jboss.as.controller.client.helpers.standalone.ServerDeploymentPlanRes
 
 /**
  * A class that holds the status of a deployment operation.
- * 
+ *
  * @author André Dietisheim
  *
  */
 public class DeploymentOperationResult {
-	
-	private Future<ServerDeploymentPlanResult> planResult;
-	private DeploymentAction action;
-	private long timeout;
-	private TimeUnit unit;
 
-	DeploymentOperationResult(DeploymentAction action, Future<ServerDeploymentPlanResult> planResult) {
-		this.action = action;
-		this.planResult = planResult;
-	}
-	DeploymentOperationResult(DeploymentAction action, Future<ServerDeploymentPlanResult> planResult, long timeout, TimeUnit unit) {
-		this(action, planResult);
-		this.timeout = timeout;
-		this.unit = unit;
-	}
+    private Future<ServerDeploymentPlanResult> planResult;
+    private DeploymentAction action;
+    private long timeout;
+    private TimeUnit unit;
 
-	public ServerDeploymentActionResult getStatus() throws Exception {
-		try {
-			ServerDeploymentActionResult actionResult = null;
-			if( unit == null )
-				actionResult = planResult.get().getDeploymentActionResult(action.getId());
-			else
-				actionResult = planResult.get(timeout, unit).getDeploymentActionResult(action.getId());
-			return actionResult;
-		} catch (Exception e) {
-			throw e;
-		}
-	}
+    DeploymentOperationResult(DeploymentAction action, Future<ServerDeploymentPlanResult> planResult) {
+        this.action = action;
+        this.planResult = planResult;
+    }
 
-	/*
-	 * Candidate for API
-	 */
-	public boolean isDone() {
-		return planResult.isDone();
-	}
-	
-	/*
-	 * Candidate for API
-	 */
-	public void cancel() {
-		planResult.cancel(true);
-	}
+    DeploymentOperationResult(DeploymentAction action, Future<ServerDeploymentPlanResult> planResult, long timeout,
+            TimeUnit unit) {
+        this(action, planResult);
+        this.timeout = timeout;
+        this.unit = unit;
+    }
+
+    public ServerDeploymentActionResult getStatus() throws Exception {
+        try {
+            ServerDeploymentActionResult actionResult = null;
+            if (unit == null)
+                actionResult = planResult.get().getDeploymentActionResult(action.getId());
+            else
+                actionResult = planResult.get(timeout, unit).getDeploymentActionResult(action.getId());
+            return actionResult;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    /*
+     * Candidate for API
+     */
+    public boolean isDone() {
+        return planResult.isDone();
+    }
+
+    /*
+     * Candidate for API
+     */
+    public void cancel() {
+        planResult.cancel(true);
+    }
 }
--- src/main/java/org/wildfly/demo/wildfly11client/IncrementalManagementModel.java
+++ src/main/java/org/wildfly/demo/wildfly11client/IncrementalManagementModel.java
@@ -1,4 +1,4 @@
-
+package org.wildfly.demo.wildfly11client;
 
 import java.util.HashMap;
 import java.util.List;
@@ -6,47 +6,47 @@ import java.util.Map;
 import java.util.Set;
 
 public class IncrementalManagementModel {
-	
-	private HashMap<String, Module> map;
-	public IncrementalManagementModel() {
-		map = new HashMap<String, Module>();
-	}
-	
-	public void put(String id, Map<String, String> changedContent, List<String> removedContent) {
-		Module m = new Module(changedContent, removedContent);
-		map.put(id,  m);
-	}
-	
-	public String[] keys() {
-		Set<String> s = map.keySet();
-		return (String[]) s.toArray(new String[s.size()]);
-	}
-	
-	public Map<String,String> getChanged(String id) {
-		Module m = map.get(id);
-		if( m != null ) {
-			return m.changedContent;
-		}
-		return null;
-	}
 
-	public List<String> getRemoved(String id) {
-		Module m = map.get(id);
-		if( m != null ) {
-			return m.removedContent;
-		}
-		return null;
-	}
+    private HashMap<String, Module> map;
 
-	
-	
-	private static class Module {
-		Map<String, String> changedContent;
-		List<String> removedContent;
-		public Module(Map<String, String> changedContent, List<String> removedContent) {
-			this.changedContent = changedContent; 
-			this.removedContent = removedContent;
-		}
-	}
+    public IncrementalManagementModel() {
+        map = new HashMap<String, Module>();
+    }
+
+    public void put(String id, Map<String, String> changedContent, List<String> removedContent) {
+        Module m = new Module(changedContent, removedContent);
+        map.put(id, m);
+    }
+
+    public String[] keys() {
+        Set<String> s = map.keySet();
+        return (String[]) s.toArray(new String[s.size()]);
+    }
+
+    public Map<String, String> getChanged(String id) {
+        Module m = map.get(id);
+        if (m != null) {
+            return m.changedContent;
+        }
+        return null;
+    }
+
+    public List<String> getRemoved(String id) {
+        Module m = map.get(id);
+        if (m != null) {
+            return m.removedContent;
+        }
+        return null;
+    }
+
+    private static class Module {
+        Map<String, String> changedContent;
+        List<String> removedContent;
+
+        public Module(Map<String, String> changedContent, List<String> removedContent) {
+            this.changedContent = changedContent;
+            this.removedContent = removedContent;
+        }
+    }
 
 }
